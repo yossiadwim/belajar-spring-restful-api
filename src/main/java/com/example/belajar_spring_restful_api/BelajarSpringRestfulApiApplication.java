078d88677426fbaa1695dfda958e418cd055c4a0
Yossia Dwi Mahardika <yossia.dwi@ti.ukdw.ac.id>
2025-03-08 18:59:05 +0700

    Initial commit

---
+++ src/main/java/com/example/belajar_spring_restful_api/BelajarSpringRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/belajar_spring_restful_api/BelajarSpringRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
